1bb762047528bdb3459e650cfb1606cb732427de
Dhanush-Proj <dhanushpreji5@gmail.com>
2025-11-21 13:14:48 +0530

    changed everything

--- src/main.js
+++ src/main.js
@@ -1,42 +1,59 @@
+const nameText = "Dhanush P Reji";
+let i = 0;
 
-$('#navigation a').on('click', function() {
-  var hash = this.hash;
-  $('html, body').animate({
-    scrollTop: $(this.hash).offset().top
-  }, 1000);
-});
-
-
-$('.toggler, .nav-content a:not(#dropdown-link)').on('click', function(){
-  $('.toggler').toggleClass('change');
-  $('.nav-content').slideToggle();
-  $('#dropdown-menu').slideUp();
-  $('.menu-overlay').toggle();
-});
-
-$('.nav-content .dropdown').on('click', function(){
-  $('#dropdown-menu').slideToggle();
-});
-
-$('.menu-overlay').on('click', function(){
-  $('.toggler').removeClass('change');
-  $('.nav-content').slideUp();
-  $('#dropdown-menu').slideUp();
-  $('.menu-overlay').hide();
-});
-
-$("#contact input, #contact textarea").on('focusout', function(){
-
-  var text_val = $(this).val();
-  if (text_val === "") {
-    $(this).removeClass('has-value');
-  } else {
-    $(this).addClass('has-value');
+document.addEventListener("DOMContentLoaded", () => {
+  const nameEl = document.getElementById("name");
+  function typeName() {
+    if (i < nameText.length) {
+      nameEl.textContent += nameText.charAt(i);
+      i++;
+      setTimeout(typeName, 100);
+    }
   }
+  typeName();
 
+  // Blast animation for background icons
+  document.querySelectorAll('.bg-icon').forEach(icon => {
+    const x = Math.random() * window.innerWidth - window.innerWidth / 2;
+    const y = Math.random() * window.innerHeight - window.innerHeight / 2;
+    icon.style.left = '50%';
+    icon.style.top = '50%';
+    icon.style.setProperty('--x', `${x}px`);
+    icon.style.setProperty('--y', `${y}px`);
+    requestAnimationFrame(() => icon.classList.add('blast'));
+  });
+
+// Image click preview
+const modal = document.getElementById("imgModal");
+const modalImg = document.getElementById("modalImg");
+
+document.querySelectorAll(".project-image").forEach(img => {
+  img.addEventListener("click", (e) => {
+    modalImg.src = e.target.src;
+    modal.style.display = "flex";
+  });
 });
-// document.getElementById('downloadbtn').addEventListener('click',function(){
-//   window.location.href = "https://drive.google.com/uc?export=download&id=1OVOyQLRrmGQ7yr3UzPGuILc5P73wNp-v";
-//   window.location.reload();
-// })
+});
 
+function closeImage() {
+  document.getElementById("imgModal").style.display = "none";
+}
+
+
+    // Tiny JS enhancement: active nav link on scroll
+    const sections = document.querySelectorAll("section, header");
+    const navLinks = document.querySelectorAll("nav a");
+
+    window.addEventListener("scroll", () => {
+      let current = "home";
+      sections.forEach(sec => {
+        const top = window.scrollY;
+        if (sec.offsetTop - 120 <= top) current = sec.id;
+      });
+
+      navLinks.forEach(a => {
+        a.style.color = a.getAttribute("href") === `#${current}`
+          ? "var(--yellow)"
+          : "var(--fg)";
+      });
+    });

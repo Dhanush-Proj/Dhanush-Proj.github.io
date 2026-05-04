629a648811c8959997f20cdb786f2d84ef27abcc
Dhanush-Proj <dhanushpreji5@gmail.com>
2026-05-04 11:09:41 +0530

    added copyright

--- src/main.js
+++ src/main.js
@@ -81,3 +81,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }, { once: true });
   });
 });
+
+
+const currentYearEl = document.getElementById("currentYear");
+if (currentYearEl) {
+  currentYearEl.textContent = new Date().getFullYear();
+
+}
